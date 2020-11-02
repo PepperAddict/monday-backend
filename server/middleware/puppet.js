1003e1e4b73808fa11224d5b2304b2da687ebb45
unknown <jenearly@gmail.com>
2020-11-02 12:58:44 -0500

    using playwright now

--- server/middleware/puppet.js
+++ server/middleware/puppet.js
@@ -1,66 +1,59 @@
 const express = require("express");
 require("dotenv").config();
+const { firefox, chromium} = require("playwright");
 
 const router = express();
-const puppeteer = require("puppeteer");
 
-router.get(
-  ["/api/1/puppeteer/", "/api/1/puppeteer/:page?"],
-  async (req, res) => {
+router.get(["/api/1/play/", "/api/1/play/:page?"], async (req, res) => {
+
     const whichView = {
-      mobile: {
-        width: 375,
-        height: 667,
-        deviceScaleFactor: 2,
-        isMobile: true,
-        hasTouch: true,
-        isLandscape: false,
-      },
-      tablet: {
-        width: 768,
-        height: 1024,
-        deviceScaleFactor: 2,
-        isMobile: true,
-        hasTouch: true,
-        isLandscape: false,
-      },
-      desktop: {
-        width: 1200,
-        height: 800,
-      },
-    };
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
+        mobile: {
+          width: 375,
+          height: 667,
+          deviceScaleFactor: 2,
+          isMobile: true,
+          hasTouch: true,
+          isLandscape: false,
+        },
+        tablet: {
+          width: 768,
+          height: 1024,
+          deviceScaleFactor: 2,
+          isMobile: true,
+          hasTouch: true,
+          isLandscape: false,
+        },
+        desktop: {
+          width: 1200,
+          height: 800,
+        },
+      };
 
-    const browser = await puppeteer.launch({
-      args: ["-no-sandbox", "--disable-setuid-sandbox"],
-    });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  const args = [
+    '--disable-gpu',
+    '--no-sandbox',
+    '--disable-web-security',
+    '--disable-dev-profile',
+  ]
 
-    try {
-      const page = await browser.newPage();
+  const browser = (req.query.full) ? await firefox.launch({args}) : await chromium.launch({args});
+  const page = await browser.newPage();
 
-      await page.setViewport(whichView[req.query.mode || "desktop"]);
-      await page.goto(req.query.url, {
-        waitUntil: "networkidle2",
-        timeout: 60000,
-      });
-
-      const image = await page.screenshot({
-        type: "png",
-        fullPage: req.query.full === "yes" ? true : false,
-      });
-      await browser.close();
-      res.set("Content-Type", "image/png");
-      res.send(image);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-);
+  await page.setViewportSize(whichView[req.query.mode || "desktop"])
+  await page.goto(req.query.url);
+  
+  const image = await page.screenshot({type: 'png', fullPage: req.query.full === "yes" ? true : false,
+})
+  await browser.close();
+  res.set("Content-Type", "image/png");
+  res.send(image);
+});
 
 module.exports = router;
